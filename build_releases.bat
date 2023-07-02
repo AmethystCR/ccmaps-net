451d64caa50ac627027ec127b6cb6ed0b2e8d7f6
霜序零一 <942708116@qq.com>
2023-07-02 17:40:38 +0800

    Update build_releases.bat

--- build_releases.bat
+++ build_releases.bat
@@ -1,5 +1,5 @@
 set VER=2.4.2
-set MAKENSIS="%PROGRAMFILES(X86)%\nsis\makensis.exe"
+set MAKENSIS="%PROGRAMFILES%\nsis\makensis.exe"
 
 del CNCMaps_release_*.exe
 del CNCMaps_debug_*.exe
@@ -9,12 +9,12 @@ rmdir /s /q CNCMaps.Renderer\bin\
 rmdir /s /q CNCMaps.Renderer.GUI\bin\
 rmdir /s /q CNCMaps.Shared\bin\
 
-set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Enterprise\"
+set "VSINSTALLDIR=%PROGRAMFILES%\Microsoft Visual Studio\2022\Enterprise\"
 if not exist "%VSINSTALLDIR%" (
-set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Professional\"
+set "VSINSTALLDIR=%PROGRAMFILES%\Microsoft Visual Studio\2022\Professional\"
 )
 if not exist "%VSINSTALLDIR%" (
-set "VSINSTALLDIR=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2019\Community\"
+set "VSINSTALLDIR=%PROGRAMFILES%\Microsoft Visual Studio\2022\Community\"
 )
 set DevEnvDir=%VSINSTALLDIR%Common7\IDE\
 call "%VSINSTALLDIR%Common7\Tools\VsDevCmd.bat"
